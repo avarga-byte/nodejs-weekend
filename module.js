f5a64a6a1d28ffdaa5d138131b560e2f4d9270e6
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-02-12 01:23:52 -0500

    budget front

---
+++ module.js
@@ -0,0 +1,26 @@
+export let object = {
+    ner: "Narada",
+    email: "narada@mail.com"
+}
+
+export let x = 5;
+
+export function test(){
+    console.log('this function was imported')
+}
+
+export default function bla (){
+    console.log("this is default export function")
+};
+
+// default export
+// named export
+
+// export , import
+
+/*
+{
+    object: object,
+    x: x
+}
+*/
---
+++ script.js
@@ -0,0 +1,55 @@
+// // import { object , x , test } from "./module.js";
+// // import bla from './module.js';
+
+// // test();
+
+// // console.log(object , x);
+
+// // bla()
+
+// // IIFE - Imediately invoked function expression
+// // OOP 
+
+// // prototypical
+// // methodical
+// class User {
+//     constructor(ner , mail){
+//         this.ner = ner;
+//         this.mail = mail
+//     }
+
+//     getName(){
+//         return this.ner
+//     }
+
+//     setName(ner){
+//         this.ner = ner
+//         return this.ner
+//     }
+
+//     static test(){
+//         console.log('hi')
+//     }
+// }
+
+// const user = new User("Narada" , "narada@mail.com");
+// const user1 = new User("John" , "john@mail.com");
+
+// // console.log(user.statusCheck());
+
+
+// // class - blueprint of object
+
+// class VipUser extends User {
+//     constructor(ner , email , status){
+//         super(ner , email);
+//         this.status = status;
+//     }
+
+//     statusCheck(){
+//         console.log('hi')
+//     }
+// }
+
+// const vip = new VipUser("James" , "james@mail.com" , "premium");
+// console.log(vip.statusCheck())
